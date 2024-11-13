a1663234b166994bb3f690a89bf929b1cb63b59c
Sabyasachi Ghosh <sabyasachighosh020803@gmail.com>
2024-11-13 20:12:37 +0530

    c learnig

---
+++ firstprogramming.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x = 15;
+    int y = 7;
+int z = x + y;
+    printf("%d",x+y);
+     printf("\n%d",x-y);
+      printf("\n%d",x*y);
+       printf("\n%d",x/y);
+       printf("\n%d",z);
+    return 0;
+}
